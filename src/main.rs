0156451bf63011d5d4f75219434180012ee42c24
Benjamin Aaron <ben@theaarons.com>
2024-09-20 17:35:06 -0700

    Tokenizer

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,18 @@ enum Atom {
     Number(Number),
 }
 
+impl From<String> for Atom {
+    fn from(s: String) -> Self {
+        if let Ok(i) = s.parse::<i64>() {
+            Atom::Number(Number::Integer(i))
+        } else if let Ok(fl) = s.parse::<f64>() {
+            Atom::Number(Number::Float(fl))
+        } else {
+            Atom::Symbol(s)
+        }
+    }
+}
+
 #[derive(Clone, PartialEq)]
 enum Expr {
     Atom(Atom),
@@ -44,6 +56,40 @@ impl fmt::Display for Expr {
     }
 }
 
+fn tokenize(input: &str) -> Vec<String> {
+    input
+        .replace("(", " ( ")
+        .replace(")", " ) ")
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect()
+}
+
+fn parse(program: &str) -> Expr {
+    read_from_tokens(&mut tokenize(program).into_iter().rev().collect())
+}
+
+fn read_from_tokens(tokens: &mut Vec<String>) -> Expr {
+    if let Some(token) = tokens.pop() {
+        match token.as_str() {
+            "(" => {
+                let mut list = Vec::new();
+                while tokens.last().map_or(false, |t| t != ")") {
+                    list.push(read_from_tokens(tokens));
+                }
+                tokens.pop();
+                Expr::List(list)
+            }
+            ")" => panic!("unexpected ')'"),
+            _ => Expr::Atom(Atom::from(token)),
+        }
+    } else {
+        panic!("unexpected EOF while reading");
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let program = "(+ 1 2)";
+    println!("{}", program);
+    println!("{:?}", tokenize(program));
+    println!("{}", parse(program));
 }
